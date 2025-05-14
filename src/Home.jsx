be57a73eaad9c0f16ae1f8b844f196a8561cd7bf
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-05-14 22:49:24 +0200

    Refactor code structure for improved readability and maintainability

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
 import './Home.css';
+import meImage from '../public/me.jpg';
 
 function Home() {
   return (
     <div id="home">
       <h3>WELCOME TO MY WEBSITE</h3>
-      <h1>I'm Václav Vyrobík a Software Developer.</h1>
+      <h1>I'm <span style={{color: '#FF6F61'}}>Václav Vyrobík</span> a Software Developer.</h1>
     </div>
   );
 }
