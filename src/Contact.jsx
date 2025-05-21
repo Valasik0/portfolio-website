8492299779c314aeb7ee941b7b5a50c50f8f8d02
Vaclav Vyrobik <vaclav.vyrobik@gmail.com>
2025-05-21 12:26:36 +0200

    Add contact form and update About section with additional content

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,16 @@
 import './Contact.css';
+import './ContactForm.css';
 
 function Contact() {
   return (
     <div id="contact">
       <h1>Contact</h1>
-      <p>This is the contact section.</p>
+      <form className="contact-form">
+          <input placeholder='Name' type="text" name="name" required />
+          <input placeholder='Email' type="email" name="email" required />
+          <textarea placeholder='Message' name="message" rows="5" required></textarea>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
